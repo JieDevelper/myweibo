02a7b44c65fef8d003c309d27dbeeeb341cd940e
ZhangJie <zhangjie@baiwenbao.com>
2016-03-10 21:16:48 +0800

    Initial Commit

---
+++ myWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myWeibo
+//
+//  Created by zhangjie520 on 3/10/16.
+//  Copyright © 2016 zhangjie520. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myWeibo
+//
+//  Created by zhangjie520 on 3/10/16.
+//  Copyright © 2016 zhangjie520. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
